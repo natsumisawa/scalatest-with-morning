cc9efe383670d20988caf7756006d24d35349c60
abab <ababup1192@gmail.com>
2018-10-23 03:44:07 +0900

    怒涛の修正 + リファクタリング + コメント

--- morning-coffee/src/test/scala/example/MorningSpec.scala
+++ morning-coffee/src/test/scala/example/MorningSpec.scala
@@ -5,43 +5,50 @@ import org.scalatest.mockito.MockitoSugar
 
 import scala.concurrent.Future
 import example.repositry._
-import example.service.MorningMusicService
+import example.service.{MorningMusicService, Title}
 
 class MorningSpec extends AsyncFunSpec with Matchers with MockitoSugar {
 
-  case class Title(value: String, isTsunku: Boolean)
-
   describe("getNewMusic") {
-    it("新曲が取得できる") {
-      val titles = Seq("自由な国だから", "憧れのStress-Free", "フラリ銀座")
+    it("新曲のリストが取得できる") {
+      val expected = Seq("自由な国だから", "憧れのStress-Free", "フラリ銀座")
       val morningMusicRepo = mock[MorningMusicRepository]
-      when(morningMusicRepo.list()).thenReturn(Future(titles))
       val morningMusic = new MorningMusicService(morningMusicRepo)
-      val titlesFuture = morningMusic.getNewTitles()
-      titlesFuture.map(title => {
-        assert(title == titles)
+
+      when(morningMusicRepo.list()).thenReturn(Future(expected))
+
+      val titlesFuture = morningMusic.getNewTitles
+      // RepoをMockして、そのまま値を返すかどうかのテストを書く場合 意味の薄いテストになってしまうので
+      // せめてverifyで そのメソッドが1回(バッチとかだったらN回) 動くことをテストする。
+      verify(morningMusicRepo, times(1)).list()
+      titlesFuture.map(actual => {
+        assert(actual == expected)
       })
     }
   }
 
   describe("isTsunku") {
-    // TODO なんでこれ落ちるの・・・
-    it("つんく曲だったらtrueで返す") {
-      val titles = Seq("自由な国だから", "憧れのStress-Free", "フラリ銀座", "泡沫サタデーナイト", "ナルシスかまってちゃん協奏曲")
+    // タイトルのリスト(List String) で良さそうだけども・・・
+    it("つんく曲だったら真偽値を含んだタイトルのリストで返す") {
+      // 何のタイトル化わからなかったので titles -> findTitles
+      val findTitles = Seq("自由な国だから", "憧れのStress-Free", "フラリ銀座", "泡沫サタデーナイト", "ナルシスかまってちゃん協奏曲")
       val morningMusicRepo = mock[MorningMusicRepository]
       val morningMusic = new MorningMusicService(morningMusicRepo)
-      val expectedResult = Seq(
-        Title("自由な国だから", true),
-        Title("憧れのStress-Free", true),
-        Title("フラリ銀座", true),
-        Title("泡沫サタデーナイト", false),
-        Title("ナルシスかまってちゃん協奏曲", true)
+      // true, false だけ並んでいても読んだ人がわからないので、名前付きにする
+      val expected = Seq(
+        Title("自由な国だから", isTsunku = true),
+        Title("憧れのStress-Free", isTsunku = true),
+        Title("フラリ銀座", isTsunku = true),
+        Title("泡沫サタデーナイト", isTsunku = false),
+        Title("ナルシスかまってちゃん協奏曲", isTsunku = true)
       )
+
       val tsunkuList = Seq("自由な国だから", "憧れのStress-Free", "フラリ銀座", "ナルシスかまってちゃん協奏曲")
-      when(morningMusicRepo.listByTsunku(titles)).thenReturn(Future(tsunkuList))
-      val resultFuture = morningMusic.isTsunku(titles)
-      resultFuture.map(result => {
-        assert(result == expectedResult)
+      when(morningMusicRepo.listByTsunku(findTitles)).thenReturn(Future(tsunkuList))
+
+      val resultFuture = morningMusic.isTsunku(findTitles)
+      resultFuture.map(actual => {
+        assert(actual == expected)
       })
     }
   }
